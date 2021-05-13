eb9252f0885091a5ae008863ccb19e49ae2554ae
gayoung.koh <gayoung.koh@qcells.com>
2021-05-13 17:09:46 +0900

    created vue3-project

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="name">{{ name }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = 'Dev Flowing';
+
+    return  {
+      name
+    };
+  },
+}
+</script>
+
+<style scoped>
+  .name {
+    color: red;
+  }
+</style>
