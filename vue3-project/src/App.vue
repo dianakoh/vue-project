8e58f56254f8d3c565542ec2b134fde78a703d65
gayoung.koh <gayoung.koh@qcells.com>
2021-05-14 10:59:46 +0900

    Chapter 9
    - 양방향 바인딩(v-model)

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="name">{{ name }}</div>
+  <input 
+  type="text" 
+  v-model="name">
+  <button 
+  class="btn btn-primary"
+  @click="onSubmit"
+  >
+  Click
+  </button>
 </template>
 
 <script>
+import {ref} from 'vue';
+
 export default {
   setup() {
-    const name = 'Dev Flowing';
+    const name = ref('Dev');
+    // const greeting = (name) => {
+    //     return 'Hello, ' + name;
+    // };
+
+    // const greet = greeting(name);
+
+    const onSubmit = () => {
+      console.log(name.value)
+    };
 
     return  {
-      name
+      name,
+      onSubmit,
     };
   },
 }
