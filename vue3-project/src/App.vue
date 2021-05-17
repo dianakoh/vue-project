d5b7255e8ef3ca14e344dbb77703e111593d5872
gayoung.koh <gayoung.koh@qcells.com>
2021-05-17 13:04:29 +0900

    To-Do db에서 삭제하기

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -67,8 +67,16 @@ export default {
       todos.value[index].completed = !todos.value[index].completed;
     };
 
-    const deleteTodo = (index) => {
-      todos.value.splice(index, 1);
+    const deleteTodo = async (index) => {
+      error.value = '';
+      const id = todos.value[index].id;
+      try {
+        await axios.delete('http://localhost:3000/todos/' + id);
+        todos.value.splice(index, 1);
+      } catch (err) {
+        error.value = 'Something went wrong.';
+      }
+      
     };
 
     const searchText = ref('');
