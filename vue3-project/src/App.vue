12da2cfaa7e1abf2c1edfb8cb3c9fada05ef55bb
gayoung.koh <gayoung.koh@qcells.com>
2021-05-17 10:30:39 +0900

    To-Do 생성시 db에 저장하기

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -9,6 +9,7 @@
     >
     <hr />
     <TodoSimpleForm @add-todo="addTodo"/>
+    <div style="color: red">{{ error }}</div>
 
     <div v-if="!filteredTodos.length">
       There is nothing to display
@@ -25,6 +26,7 @@
 import {ref, computed} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from "./components/TodoList.vue";
+import axios from 'axios';
 
 export default {
   components: {
@@ -34,9 +36,22 @@ export default {
   setup() {
 
     const todos = ref([]);
+    const error = ref('');
   
     const addTodo = (todo) => {
-      todos.value.push(todo);
+      // 데이터베이스에 todo 저장
+      error.value = '';
+      axios.post('http://localhost:3000/todos', {
+        subject: todo.subject,
+        completed: todo.completed,
+      }).then(res => {
+        console.log(res);
+        todos.value.push(res.data);
+      }).catch(err => {
+        console.log(err);
+        error.value = 'Something went wrong.'
+      });
+      
     };
 
     const toggleTodo = (index) => {
@@ -63,7 +78,8 @@ export default {
       addTodo,
       toggleTodo,
       searchText,
-      filteredTodos
+      filteredTodos,
+      error
     };
   },
 }
