7ace6581e4dde97057f9180e04833d41e65c727b
gayoung.koh <gayoung.koh@qcells.com>
2021-05-23 22:20:35 +0900

    작은 문제점들 수정

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -57,7 +57,7 @@ export default {
       currentPage.value = page;
       try {
         const res = await axios.get(
-          `http://localhost:3000/todos?subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
         );
         numberOfTodos.value = res.headers["x-total-count"];
         todos.value = res.data;
@@ -72,11 +72,11 @@ export default {
       // 데이터베이스에 todo 저장
       error.value = "";
       try {
-        const res = await axios.post("http://localhost:3000/todos", {
+        await axios.post("http://localhost:3000/todos", {
           subject: todo.subject,
           completed: todo.completed,
         });
-        todos.value.push(res.data);
+        getTodos(1);
       } catch (err) {
         error.value = "Something went wrong.";
       }
@@ -101,7 +101,7 @@ export default {
       const id = todos.value[index].id;
       try {
         await axios.delete("http://localhost:3000/todos/" + id);
-        todos.value.splice(index, 1);
+        getTodos(1);
       } catch (err) {
         error.value = "Something went wrong.";
       }
