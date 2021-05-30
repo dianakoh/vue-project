1afa2c998285591117e4888f3f04a6eebaf7befc
gayoung.koh <gayoung.koh@qcells.com>
2021-05-31 00:40:13 +0900

    Navbar 추가하기

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,4 +1,13 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">To-Do Example</a>
+
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Todos</a>
+      </li>
+    </ul>
+  </nav>
   <router-view />
 </template>
 
