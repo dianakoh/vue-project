9cd1ebcfc9afc9b40faaffc0bb3dbf6c981e7edf
gayoung.koh <gayoung.koh@qcells.com>
2021-05-31 00:56:49 +0900

    To-Do 수정 페이지 route 추가

---
+++ vue3-project/src/pages/todos/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Todo page</div>
+</template>
+<script>
+import { useRoute } from "vue-router";
+export default {
+  setup() {
+    const route = useRoute();
+
+    console.log(route.params.id);
+  },
+};
+</script>
+<style></style>
