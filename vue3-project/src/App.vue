a2946c66150a320c7e545ef4e4a6362a62b1a477
gayoung.koh <gayoung.koh@qcells.com>
2021-05-31 00:44:51 +0900

    router-link

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">To-Do Example</a>
+    <router-link class="navbar-brand" to="/">
+      To-Do Example
+    </router-link>
 
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Todos</a>
+        <router-link class="nav-link active" aria-current="page" to="/todos">
+          Todos
+        </router-link>
       </li>
     </ul>
   </nav>
